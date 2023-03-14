b5058b62fa36ca0baa43f66b49a004a7ca7012d8
AlinaS13 <117362222+AlinaS13@users.noreply.github.com>
2023-03-14 18:42:42 +0200

    add start files

---
+++ src/components/phonebook/Phonebook.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+import { Input } from 'components/input';
+
+export const Phonebook = ({ onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <Input onSubmit={onSubmit} />
+    </form>
+  );
+};
+
+Phonebook.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
