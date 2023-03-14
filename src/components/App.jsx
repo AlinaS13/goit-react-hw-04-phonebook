bc9b624fb97b20d20f252e9adaa784fdc8c5cd82
AlinaS13 <117362222+AlinaS13@users.noreply.github.com>
2023-03-14 21:57:39 +0200

    add phonebook task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,90 +1,69 @@
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Wraper } from './App.styled';
 import { Phonebook } from './phonebook';
 import { Section } from './section';
 import { Contacts } from './contacts';
 import { FilterContacts } from './filter-contacts';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
     const query = event.target.elements.name.value;
     const number = event.target.elements.number.value;
-    const findeItem = this.state.contacts.find(
-      contact => contact.name === query
-    );
-    if (findeItem) {
+    const findItem = contacts.find(contact => contact.name === query);
+
+    if (findItem) {
       alert(`${query} is alredy in contacts`);
     } else {
-      this.setState(prevState => ({
-        ...prevState,
-        contacts: prevState.contacts.concat({
+      setContacts(prevContacts => [
+        ...prevContacts,
+        {
           name: query,
           id: nanoid(),
           number: number,
-        }),
-      }));
+        },
+      ]);
     }
     event.target.reset();
   };
 
-  handleChengeInput = str => {
-    this.setState(prevState => ({
-      filter: str,
-    }));
+  const handleChengeInput = str => {
+    setFilter(str);
   };
 
-  applyFilters = () => {
-    return this.state.contacts.filter(
+  const applyFilters = () => {
+    return contacts.filter(
       contact =>
-        contact.name.toLowerCase().includes(this.state.filter.toLowerCase()) ||
-        contact.number.includes(this.state.filter)
+        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+        contact.number.includes(filter)
     );
   };
-  handleDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const handleDeleteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  render() {
-    const { filter } = this.state;
-    return (
-      <Wraper>
-        <Section title="Phone book">
-          <Phonebook onSubmit={this.handleSubmit} />
-        </Section>
-        <Section title="Contacts">
-          <FilterContacts
-            filter={filter}
-            onChangeInput={this.handleChengeInput}
-          />
-          <Contacts
-            onSubmit={this.handleSubmit}
-            contacts={this.applyFilters()}
-            onDeleteContact={this.handleDeleteContact}
-          />
-        </Section>
-      </Wraper>
-    );
-  }
-}
+  return (
+    <Wraper>
+      <Section title="Phone book">
+        <Phonebook onSubmit={handleSubmit} />
+      </Section>
+      <Section title="Contacts">
+        <FilterContacts filter={filter} onChangeInput={handleChengeInput} />
+        <Contacts
+          contacts={applyFilters()}
+          onDeleteContact={handleDeleteContact}
+        />
+      </Section>
+    </Wraper>
+  );
+};
